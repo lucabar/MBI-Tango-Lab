3656f45615701bddff412354fd3f4d8c33b7e9e0
lucabar <lucabarbera@posteo.de>
2020-06-01 20:12:32 +0200

    added gate function

--- NI-USB_TDS/ni_usb_TDS.py
+++ NI-USB_TDS/ni_usb_TDS.py
@@ -8,8 +8,8 @@ Created on Wed May 27 14:17:29 2020
 
 """
 from tango import AttrWriteType, DevState, DebugIt
-from tango.server import Device, attribute
-
+from tango.server import Device, attribute, command
+import time
 import ni_usb_6501 as ni
 
 
@@ -52,19 +52,6 @@ class NIUSB6501(Device):
                 #the init method is called more than once)
                 self.info_stream('Connection has already been established in a prior run.')
                 
-    @DebugIt(show_args=True,show_kwargs=True,show_ret=True)            
-    def change_port(self, port, state):
-        
-        new = -1-port
-        self.__active=list(self.__active)
-        
-        if state:    
-            self.__active[new] = '1'
-        if not state:
-            self.__active[new] = '0'
-        self.__active = ''.join(self.__active)
-        self.dev.write_port(1,int(self.__active,2))
-        self.__ports[port] = state
         
     def read_port3(self):
         return self.__ports[3]
@@ -95,8 +82,45 @@ class NIUSB6501(Device):
     
     def write_port7(self,state):
         self.change_port(7,state)
+
+    @DebugIt(show_args=True,show_kwargs=True,show_ret=True)            
+    def change_port(self, port, state):
+        
+        new = -1-port
+        self.__active=list(self.__active)
+        
+        if state:    
+            self.__active[new] = '1'
+        if not state:
+            self.__active[new] = '0'
+        self.__active = ''.join(self.__active)
+        self.dev.write_port(1,int(self.__active,2))
+        self.__ports[port] = state
+    
+    @DebugIt()
+    @command()    
+    def gate_timer(self,port,duration):
+        self.change_port(port,True)
+        time.sleep(duration)
+        self.change(port,False)
+
+'''
+	work in progress...
+    @DebugIt()
+    @command()
+    def pulsetrain(self,port,freq,duration):
+        start = time.time()
+        for i in range(0,freq):
+            self.gate_timer(port,1/freq)
+            time.sleep(1/freq)
+        end = time.time()
+        self.info_stream('Pulse went on for {} seconds'.format(end-start))
+'''
+            
+        
         
             
 if __name__ == "__main__":
     NIUSB6501.run_server()
             
+
